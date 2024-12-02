af0a8f1d025848f57fdfbddbc1b9e36ed1499374
Prasadbotta <prasadbotta789@gmail.com>
2024-12-02 17:50:54 +0530

    done many things

--- App.js
+++ App.js
@@ -1,22 +1,5 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", { id: "heading 1" }, "This is h1 element"),
-      React.createElement("h2", { id: "heading 2" }, "This is h2 element"),
-    ]),
-  ],
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading 1" }, "This is h1 element"),
-    React.createElement("h2", { id: "heading 2" }, "This is h2 element"),
-  ])
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", abc: "abc" },
-//   "Hello world from React!"
-// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(jsxheading);
