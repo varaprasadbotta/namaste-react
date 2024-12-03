6104493a52c6eb0ffd16c8ead5f2cefa860be82b
Prasadbotta <prasadbotta789@gmail.com>
2024-12-03 16:43:57 +0530

    added header and logo and navitems

--- App.js
+++ App.js
@@ -1,5 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/*
+    Now we are going to build Food ordering app
+    ============================================
+    Planning the app
+
+    -Header
+        - logo
+        - nav-items
+    -Body
+        - search bar
+        - resturent card
+            - image
+            - Name of res
+            - price of item
+            - cienes
+            - time
+            - rating
+    -Footer
+        - copy rights
+        - links
+        - some other stuff
+*/
+// Root element
+const MyApp = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+//header
+const Header = () => {
+  return (
+    <div className="Header">
+      <img
+        alt="logo"
+        src="https://img.freepik.com/premium-vector/online-food-order-logo-icon_61778-45.jpg"
+        className="nav-logo"
+      ></img>
+      <div className="nav-items">
+        <ul className="nav-item">
+          <li>Home</li>
+          <li>Contact us</li>
+          <li>About us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxheading);
+root.render(<MyApp />);
